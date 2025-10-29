aaff198d914f604423d83f94b8285c36f1f03cfc
Rhea0524 <ST10264009@vcconnect.edu.za>
2025-10-29 16:17:55 +0200

    pipeline added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,16 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-// Your Firebase config
+// Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCUk2qKdDbp3oXJp1IBzlbQTSJPAs4bBwg",
-  authDomain: "xamu-75371-632ed.firebaseapp.com",
-  databaseURL: "https://xamu-75371-632ed-default-rtdb.firebaseio.com",
-  projectId: "xamu-75371-632ed",
-  storageBucket: "xamu-75371-632ed.appspot.com",
-  messagingSenderId: "851420243803",
-  appId: "1:851420243803:web:8e51157b0b3c4cf9ff1c7e",
-  measurementId: "G-7EV033WFZE"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -20,4 +20,3 @@ const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
 export const auth = getAuth(app);
 export default app;
-
